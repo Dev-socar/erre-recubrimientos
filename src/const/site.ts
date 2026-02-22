4fbe5c41273c3c3bad3d13b1b38465237736c8e9
Dev-socar <omar.fsc19@gmail.com>
2026-02-21 21:21:39 -0600

    feat:Se implemento el SEO a la pagina web

---
+++ src/const/site.ts
@@ -0,0 +1,7 @@
+export const site = {
+  name: "ERRE Recubrimientos",
+  url: "https://erre-recubrimientos.vercel.app/",
+  description:
+    "ERRE Recubrimientos es una empresa especializada en recubrimientos para casa-habitación y comercios. Ofrecemos aplicación de pasta, chukum, pintura, impermeabilizante, plomería y electricidad.",
+  image: "/assets/logos/logotipo.png",
+};
