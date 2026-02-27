8ce74ae01eaea936942e7dd1713ec51438bc79fc
Dev-socar <omar.fsc19@gmail.com>
2026-02-27 02:19:58 -0600

    fix: Se arreglo la url canonica

--- src/const/SiteInformation.ts
+++ src/const/SiteInformation.ts
@@ -1,6 +1,6 @@
 export const SiteInformation = {
   name: "ERRE Recubrimientos",
-  url: "https://www.errerecubrimientos.com",
+  url: "https://errerecubrimientos.com",
   description:
     "ERRE Recubrimientos es una empresa especializada en recubrimientos para casa-habitación y comercios. Ofrecemos aplicación de pasta, chukum, pintura, impermeabilizante, plomería y electricidad.",
   image: "/assets/logos/logotipo.png",
